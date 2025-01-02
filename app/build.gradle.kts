55334472fcde5567a2609be10d911227e4d11de8
marcelosartor <marcelo_sartor@hotmail.com>
2025-01-02 14:28:41 -0300

    feat-config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,19 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("com.google.devtools.ksp")
+    alias(libs.plugins.ksp) // Carregar o plugin KSP
 }
+
+
 
 android {
     namespace = "br.com.kfgdistribuidora.aularoomdatabase"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "br.com.kfgdistribuidora.aularoomdatabase"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -38,16 +40,17 @@ android {
 }
 
 dependencies {
-    val room_version = "2.6.1"
-    // Room
-    implementation("androidx.room:room-runtime:$room_version")
-    ksp("androidx.room:room-compiler:$room_version")
+   // val room_version = "2.6.1"
 
+    //implementation("androidx.room:room-runtime:$room_version")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.ksp.devtools)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
